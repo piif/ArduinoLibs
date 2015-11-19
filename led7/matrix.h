1f86636bd208fc646ba9bfb14dfd45dc193c78c9
piif <christian_lefebvre@laposte.net>
2015-11-19 23:47:06 +0100

    from http://tronixstuff.com/2013/10/11/tutorial-arduino-max7219-led-display-driver-ic/

---
+++ led7/matrix.h
@@ -0,0 +1,7 @@
+#include <Arduino.h>
+#include "LedControl/LedControl.h"
+
+void scrollMessage(LedControl &lc, char *messageString, unsigned long scrollDelay);
+
+// test font
+void scrollFont(LedControl &lc);
