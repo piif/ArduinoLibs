d4e964e64bcd871a8affec9754b05a12509df99b
Christian Lefebvre <christian_lefebvre@laposte.net>
2023-04-15 18:12:01 +0200

    ledMatrix OK with ATtiny

--- ledMatrix/example/example.ino
+++ ledMatrix/example/example.ino
@@ -1,13 +1,25 @@
 #include <Arduino.h>
-#define HAVE_SERIAL
 
 #include "ledMatrix.h"
 
 // test MAX7219 chained led matrix 8x8xN
 
-#define MATRIX_CLK A2
-#define MATRIX_CS  A1
-#define MATRIX_DIN 7
+#if defined(__AVR_ATtiny85__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny25__)
+	#define ARDUINO_TINY
+#elif defined(__AVR_MEGA__)
+	#define ARDUINO_UNO
+    // #define HAVE_SERIAL
+#endif
+
+#ifdef ARDUINO_UNO
+    #define MATRIX_CLK A2
+    #define MATRIX_CS  A1
+    #define MATRIX_DIN A4
+#else
+    #define MATRIX_CLK 1
+    #define MATRIX_CS  4
+    #define MATRIX_DIN 0
+#endif
 
 #define WIDTH 12
 
@@ -27,12 +39,16 @@ void setup() {
         ledMatrix.sendCommand(i+1, testBuffer[i], WIDTH);
     }
     ledMatrix.flush();
+#ifdef HAVE_SERIAL
+    Serial.println("Setup OK");
+#endif
 }
 
 char buffer[25];
 byte i = 0;
 
 void loop() {
+#ifdef HAVE_SERIAL
     if (i >= 25) {
         i = 0;
     }
@@ -54,4 +70,19 @@ void loop() {
     }
     buffer[i] = c;
     i++;
+#else
+    static byte ascii=0;
+
+    delay(1000);
+
+    for (byte i=0; i<24; i++) {
+        buffer[i] = ascii+20+i;
+    }
+    buffer[24] = '\0';
+    ascii= (ascii + 15) % 108; // char table contains values from 20 to 127
+
+    ledMatrix.setup();
+    ledMatrix.drawString(0, buffer);
+    ledMatrix.flush();
+#endif
 }
